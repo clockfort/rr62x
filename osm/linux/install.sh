94bac19a8ed02ebd692dc1fe3724721dad1b1046
Chris Lockfort <clockfort@csh.rit.edu>
2012-04-20 05:46:28 -0400

    installation script now accomodates 3-series kernels

--- osm/linux/install.sh
+++ osm/linux/install.sh
@@ -10,7 +10,7 @@ case ${KERNEL_VER} in
 	OBJ=o
 	MODVER=`modinfo -f%{kernel_version} ${PWD}/${TARGETNAME}.${OBJ}`
 	;;
-	2.6 )
+	2.6 | 3.0 | 3.1 | 3.2 | 3.3)
 	OBJ=ko
 	MODVER=`modinfo -F vermagic ${PWD}/${TARGETNAME}.${OBJ} | cut -d' ' -f1`
 	;;
